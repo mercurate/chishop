1ad685ed59ae1bc266cd73d20fcf0bb00cc4fe59
Markus Törnqvist <mjt@skyhood.com>
2011-08-23 11:13:20 +0000

    app_directories.Loader

--- chishop/conf/default.py
+++ chishop/conf/default.py
@@ -96,7 +96,7 @@ SECRET_KEY = 'w_#0r2hh)=!zbynb*gg&969@)sy#^-^ia3m*+sd4@lst$zyaxu'
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
