45060c87cec77a6f02fec4404a5fe11119fd6588
mercurate <mercurate@163.com>
2012-01-17 10:29:52 +0800

    fix upload bug

--- chishop/djangopypi/views/upload.py
+++ chishop/djangopypi/views/upload.py
@@ -2,38 +2,40 @@ import os
 import shutil
 import subprocess
 
+from django.conf import settings
 from django.template import RequestContext
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.db.models.query import Q
 
-from chishop.djangopypi.models import Project, UPLOAD_TO
+from chishop.djangopypi.models import Project
 
 from chishop.djangopypi.forms import ProjectForm, ReleaseForm, UploadForm
 
-import settings
-
-
 def upload(request):
-    if request.method != 'POST':
-        return redirect('/')
+    if request.method == 'GET':
+        return render(request, 'djangopypi/upload.html', {'form': UploadForm()})
 
+    print 'upload starts!'
     form = UploadForm(request.POST, request.FILES)
     if not form.is_valid():
+        print 'not valid...'
         return redirect('/')
 
     package = request.FILES['package']
     if not os.path.exists(settings.UPLOAD):
+        print 'create dir %s', settings.UPLOAD
         os.makedirs(settings.UPLOAD)
     pkg_path = os.path.join(settings.UPLOAD, package.name)
     if os.path.exists(pkg_path):
-        return redirect('/')
-
-    f = open(pkg_path, 'wb+')
-    for chunk in package.chunks():
-        f.write(chunk)
-    f.close()
+        print '%s exists, skip save.' % pkg_path
+    else:
+        f = open(pkg_path, 'wb+')
+        for chunk in package.chunks():
+            f.write(chunk)
+        f.close()
 
     if not pkg_path.endswith(('.tar.gz', '.tgz')):
+        print 'not a tar.gz'
         return redirect('/')
 
     c = 'tar -xzf %s' % (pkg_path)
@@ -49,4 +51,5 @@ def upload(request):
             subprocess.call(cmd.split(), cwd = path)
         shutil.rmtree(path)
 
+    print 'upload done'
     return redirect('/')
